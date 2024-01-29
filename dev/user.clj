f594bab25f57355b11d276d8ce6895f62fc3601c
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2024-01-28 22:37:43 -0800

    dev deps, hiccup and criterium

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,7 @@
 (ns user
-  (:require [dev.onionpancakes.chassis.core :as c]))
+  (:require [dev.onionpancakes.chassis.core :as c]
+            [criterium.core :refer [quick-bench]]
+            [hiccup2.core :as h]))
 
 (defn page
   [data]
@@ -12,7 +14,19 @@
         [:a {:href (str "/item/" (:uuid m))}
          (:name m)]])]]])
 
+(defn hiccup-page
+  [data]
+  (h/html
+   [:html {:lang "en"}
+    [:body
+     [:h1 (:title data)]
+     [:div
+      (for [m (:items data)]
+        [:div
+         [:a {:href (str "/item/" (:uuid m))}
+          (:name m)]])]]]))
+
 (def data
-  {:items (for [i (range 200)]
+  {:items (for [i (range 100)]
             {:uuid (random-uuid)
              :name (str "Item: " i)})})
