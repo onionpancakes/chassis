cf2294de396a70c9820b105364c3f2f4b6cd542a
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2024-01-29 00:06:52 -0800

    code org; element optimizations by count

--- src/dev/onionpancakes/chassis/core.clj
+++ src/dev/onionpancakes/chassis/core.clj
@@ -7,47 +7,7 @@
 (defprotocol Token
   (fragment [this]))
 
-(extend-protocol Node
-  clojure.lang.IPersistentVector
-  (branch? [this] true)
-  (children [[tag & content]]
-    [::begin-open tag ::end-open content ::begin-close tag ::end-close]
-    #_
-    [\< tag \> content \< \/ tag \>])
-  clojure.lang.ISeq
-  (branch? [_] true)
-  (children [this] this)
-  clojure.lang.IPersistentMap
-  (branch? [_] true)
-  (children [this] [])
-  Object
-  (branch? [_] false)
-  (children [_] nil)
-  nil
-  (branch? [_] false)
-  (children [_] nil))
-
-(extend-protocol Token
-  clojure.lang.Keyword
-  (fragment [this]
-    #_
-    (name this)
-    (case this
-      ::begin-open  "<"
-      ::end-open    ">"
-      ::begin-close "</"
-      ::end-close   ">"
-      (name this)))
-  Character
-  (fragment [this] (.toString this))
-  String
-  (fragment [this] this)
-  Object
-  (fragment [this] (.toString this))
-  nil
-  (fragment [_] ""))
-
-;;
+;; Serializers
 
 (deftype TokenSerializer [root]
   clojure.lang.IReduceInit
@@ -77,3 +37,93 @@
   [root]
   (eduction (map fragment)
             (token-serializer root)))
+
+;; Node impl
+
+(defn element-children-1
+  [elem]
+  (let [tag (.nth ^clojure.lang.Indexed elem 0)]
+    [::begin-open tag ::end-open ::begin-close tag ::end-close]))
+
+(defn element-children-2
+  [elem]
+  (let [tag   (.nth ^clojure.lang.Indexed elem 0)
+        anode (.nth ^clojure.lang.Indexed elem 1)]
+    (if (map? anode)
+      [::begin-open tag anode ::end-open
+       ::begin-close tag ::end-close]
+      [::begin-open tag ::end-open
+       anode
+       ::begin-close tag ::end-close])))
+
+(defn element-children-3
+  [elem]
+  (let [tag   (.nth ^clojure.lang.Indexed elem 0)
+        anode (.nth ^clojure.lang.Indexed elem 1)]
+    (if (map? anode)
+      [::begin-open tag anode ::end-open
+       (.nth ^clojure.lang.Indexed elem 2)
+       ::begin-close tag ::end-close]
+      [::begin-open tag ::end-open
+       anode
+       (.nth ^clojure.lang.Indexed elem 2)
+       ::begin-close tag ::end-close])))
+
+(defn element-children-n
+  [elem]
+  (let [tag   (.nth ^clojure.lang.Indexed elem 0)
+        anode (.nth ^clojure.lang.Indexed elem 1)]
+    (if (map? anode)
+      [::begin-open tag anode ::end-open
+       (drop 2 elem)
+       ::begin-close tag ::end-close]
+      [::begin-open tag ::end-open
+       (drop 1 elem)
+       ::begin-close tag ::end-close])))
+
+(defn element-children
+  [elem]
+  (case (count elem)
+    0 (element-children-0 elem)
+    1 (element-children-1 elem)
+    2 (element-children-2 elem)
+    3 (element-children-3 elem)
+    (element-children-n elem)))
+
+(extend-protocol Node
+  clojure.lang.IPersistentVector
+  (branch? [this] true)
+  (children [this]
+    (element-children this))
+  clojure.lang.ISeq
+  (branch? [_] true)
+  (children [this] this)
+  clojure.lang.IPersistentMap
+  (branch? [_] true)
+  (children [this] [])
+  Object
+  (branch? [_] false)
+  (children [_] [])
+  nil
+  (branch? [_] false)
+  (children [_] []))
+
+;; Token impl
+
+(extend-protocol Token
+  clojure.lang.Keyword
+  (fragment [this]
+    (case this
+      ::begin-open  "<"
+      ::end-open    ">"
+      ::begin-close "</"
+      ::end-close   ">"
+      (name this)))
+  Character
+  (fragment [this] (.toString this))
+  String
+  (fragment [this] this)
+  Object
+  (fragment [this] (.toString this))
+  nil
+  (fragment [_] ""))
