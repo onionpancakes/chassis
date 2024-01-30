f091cdd495f4941ae27127ffa67b66e56df36e9d
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2024-01-29 18:42:54 -0800

    changed opening and closing tags as complete tokens

--- src/dev/onionpancakes/chassis/core.clj
+++ src/dev/onionpancakes/chassis/core.clj
@@ -40,40 +40,47 @@
 
 ;; Token impl
 
-(extend-protocol Token
-  clojure.lang.Keyword
+(defn append-attr-kv
+  [^StringBuilder sb k v]
+  (.. sb
+      (append " ")
+      (append (name k))
+      (append "=\"")
+      (append v)
+      (append "\"")))
+
+(deftype OpeningTag [tag attrs]
+  Token
   (fragment [this]
-    (case this
-      ::begin-open  "<"
-      ::end-open    ">"
-      ::begin-close "</"
-      ::end-close   ">"
-      (name this)))
+    (let [sb (StringBuilder.)
+          _  (.append sb "<")
+          _  (.append sb (name tag))
+          _  (reduce-kv append-attr-kv sb attrs)
+          _  (.append sb ">")]
+      (.toString sb)))
+  Node
+  (branch? [this] false)
+  (children [this] []))
+
+(deftype ClosingTag [tag]
+  Token
+  (fragment [this]
+    (.. (StringBuilder.)
+        (append "</")
+        (append (name tag))
+        (append ">")
+        (toString)))
+  Node
+  (branch? [this] false)
+  (children [this] []))
+
+(extend-protocol Token
   String
   (fragment [this] this)
   Object
   (fragment [this] (.toString this))
   nil
   (fragment [_] ""))
-
-(defn attribute-fragment-rf
-  [^StringBuilder sb k v]
-  (doto sb
-    (.append " ")
-    (.append (name k))
-    (.append "=\"")
-    (.append v)
-    (.append "\"")))
-
-(defn attribute-fragment
-  [m]
-  (let [sb (StringBuilder.)
-        _  (reduce-kv attribute-fragment-rf sb m)]
-    (.toString sb)))
-
-(extend clojure.lang.IPersistentMap
-  Token
-  {:fragment attribute-fragment})
 
 ;; Node impl
 
@@ -84,49 +91,50 @@
 (defn element-children-1
   [elem]
   (let [tag (.nth ^clojure.lang.Indexed elem 0)]
-    [::begin-open tag ::end-open ::begin-close tag ::end-close]))
+    [(OpeningTag. tag nil)
+     (ClosingTag. tag)]))
 
 (defn element-children-2-attrs
   [elem]
   (let [tag   (.nth ^clojure.lang.Indexed elem 0)
         attrs (.nth ^clojure.lang.Indexed elem 1)]
-    [::begin-open tag attrs ::end-open
-     ::begin-close tag ::end-close]))
+    [(OpeningTag. tag attrs)
+     (ClosingTag. tag)]))
 
 (defn element-children-2
   [elem]
   (let [tag (.nth ^clojure.lang.Indexed elem 0)]
-    [::begin-open tag ::end-open
+    [(OpeningTag. tag nil)
      (.nth ^clojure.lang.Indexed elem 1)
-     ::begin-close tag ::end-close]))
+     (ClosingTag. tag)]))
 
 (defn element-children-3-attrs
   [elem]
   (let [tag   (.nth ^clojure.lang.Indexed elem 0)
         attrs (.nth ^clojure.lang.Indexed elem 1)]
-    [::begin-open tag attrs ::end-open
+    [(OpeningTag. tag attrs)
      (.nth ^clojure.lang.Indexed elem 2)
-     ::begin-close tag ::end-close]))
+     (ClosingTag. tag)]))
 
 (defn element-children-3
   [elem]
   (let [tag (.nth ^clojure.lang.Indexed elem 0)]
-    [::begin-open tag ::end-open
+    [(OpeningTag. tag nil)
      (.nth ^clojure.lang.Indexed elem 1)
      (.nth ^clojure.lang.Indexed elem 2)
-     ::begin-close tag ::end-close]))
+     (ClosingTag. tag)]))
 
 (defn element-children-n
   [elem]
   (let [tag   (.nth ^clojure.lang.Indexed elem 0)
         anode (.nth ^clojure.lang.Indexed elem 1)]
     (if (map? anode)
-      [::begin-open tag anode ::end-open
+      [(OpeningTag. tag anode)
        (drop 2 elem)
-       ::begin-close tag ::end-close]
-      [::begin-open tag ::end-open
+       (ClosingTag. tag)]
+      [(OpeningTag. tag nil)
        (drop 1 elem)
-       ::begin-close tag ::end-close])))
+       (ClosingTag. tag)])))
 
 (defn element-children
   [elem]
