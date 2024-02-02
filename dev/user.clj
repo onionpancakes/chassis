2c979f74722e39c580599a55ea12989801be45a1
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2024-02-01 23:16:50 -0800

    added selmer performance comparison

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,9 @@
 (ns user
   (:require [dev.onionpancakes.chassis.core :as c]
             [criterium.core :refer [quick-bench]]
-            [hiccup2.core :as h]))
+            [hiccup2.core :as hiccup]
+            [hiccup.page :as hiccup.page]
+            [selmer.parser :as selmer]))
 
 (defn page
   [data]
@@ -22,28 +24,47 @@
                      :href  (str "/item/" (:uuid m))}
          (:name m)]])]]])
 
+(defn chassis-page
+  [data]
+  (c/html [c/doctype-html5 (page data)]))
+
 (defn hiccup-page
   [data]
-  (h/html
-   [:html {:lang "en"}
-    [:head
-     [:link {:href "/foobar1" :rel "stylesheet"}]
-     [:link {:href "/foobar2" :rel "stylesheet"}]
-     [:link {:href "/foobar3" :rel "stylesheet"}]
-     [:link {:href "/foobar4" :rel "stylesheet"}]
-     [:title (:title data)]]
-    [:body
-     [:h1 (:title data)]
-     [:div
-      (for [m (:items data)]
-        [:div.foobar
-         [:a.baz.buz {:id    (:uuid m)
-                      :class (:type m)
-                      :href  (str "/item/" (:uuid m))}
-          (:name m)]])]]]))
+  (str
+   (hiccup/html
+    {:mode :html}
+    (hiccup.page/doctype :html5)
+    [:html {:lang "en"}
+     [:head
+      [:link {:href "/foobar1" :rel "stylesheet"}]
+      [:link {:href "/foobar2" :rel "stylesheet"}]
+      [:link {:href "/foobar3" :rel "stylesheet"}]
+      [:link {:href "/foobar4" :rel "stylesheet"}]
+      [:title (:title data)]]
+     [:body
+      [:h1 (:title data)]
+      [:div
+       (for [m (:items data)]
+         [:div.foobar
+          [:a.baz.buz {:id    (:uuid m)
+                       :class (:type m)
+                       :href  (str "/item/" (:uuid m))}
+           (:name m)]])]]])))
+
+(defn hiccup-page-runtime
+  [data]
+  (hiccup/html
+   {:mode :html}
+   (hiccup.page/doctype :html5)
+   (page data)))
+
+(defn selmer-page
+  [data]
+  (selmer/render-file "selmer/page.html" data))
 
 (def data
-  {:items (for [i (range 200)]
+  {:title "Test Items"
+   :items (for [i (range 200)]
             {:type "foo"
              :uuid (random-uuid)
              :name (str "Item: " i)})})
