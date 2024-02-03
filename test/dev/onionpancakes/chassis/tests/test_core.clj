b248d4aec01a030084763f0fe584fb56265bd87c
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2024-02-03 13:59:58 -0800

    added more tests

--- test/dev/onionpancakes/chassis/tests/test_core.clj
+++ test/dev/onionpancakes/chassis/tests/test_core.clj
@@ -17,11 +17,36 @@
     (list :foo)            ":foo"
     (list :foo "bar")      ":foobar"
     
-    []                 ""
-    [:div]             "<div></div>"
-    [:div nil]         "<div></div>"
-    [:div {}]          "<div></div>"
+    []         ""
+    [:div]     "<div></div>"
+    [:div nil] "<div></div>"
+    [:div {}]  "<div></div>"
+    
     [:div {:id nil}]   "<div></div>"
     [:div {:id ""}]    "<div id=\"\"></div>"
     [:div {:id "foo"}] "<div id=\"foo\"></div>"
-    [:div {:id 0}]     "<div id=\"0\"></div>"))
+    [:div {:id 0}]     "<div id=\"0\"></div>"
+
+    [:div {:class nil}]   "<div></div>"
+    [:div {:class ""}]    "<div class=\"\"></div>"
+    [:div {:class "foo"}] "<div class=\"foo\"></div>"
+    [:div {:class 0}]     "<div class=\"0\"></div>"
+    
+    [:div {:id nil :class nil}]     "<div></div>"
+    [:div {:id "foo" :class nil}]   "<div id=\"foo\"></div>"
+    [:div {:id nil :class "foo"}]   "<div class=\"foo\"></div>"
+    [:div {:id "foo" :class "foo"}] "<div id=\"foo\" class=\"foo\"></div>"
+
+    [:div {:foo "bar"}] "<div foo=\"bar\"></div>"))
+
+(deftest test-html-void
+  (are [node s] (= (c/html node) s)
+    [:br]     "<br>"
+    [:hr]     "<hr>"
+    [:hr nil] "<hr>"
+
+    [:hr {:id "foo"}]  "<hr id=\"foo\">"
+    [:hr {:foo "bar"}] "<hr foo=\"bar\">"
+
+    [:hr nil nil]         "<hr></hr>"
+    [:hr {:id "foo"} nil] "<hr id=\"foo\"></hr>"))
