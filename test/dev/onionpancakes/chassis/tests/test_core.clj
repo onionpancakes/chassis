6433b5549195f738c4a456912557c9bba207fa1a
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2024-02-03 20:18:37 -0800

    test comments, add tests for nested

--- test/dev/onionpancakes/chassis/tests/test_core.clj
+++ test/dev/onionpancakes/chassis/tests/test_core.clj
@@ -4,11 +4,13 @@
 
 (deftest test-html
   (are [node s] (= (c/html node) s)
+    ;; Primitives, non-elements
     nil   ""
     ""    ""
     "foo" "foo"
     0     "0"
 
+    ;; List non-elements
     ()                     ""
     (list "")              ""
     (list "foo")           "foo"
@@ -16,7 +18,8 @@
     (list "foo" "bar" 123) "foobar123"
     (list :foo)            ":foo"
     (list :foo "bar")      ":foobar"
-    
+
+    ;; Vector non-elements
     []                ""
     ["foo"]           "foo"
     ["foo" "bar"]     "foobar"
@@ -24,42 +27,52 @@
     [0]               "0"
     [:foo/foo]        ":foo/foo"
     [:foo/foo 123]    ":foo/foo123"
-    
+
+    ;; Element, no attr
     [:div]    "<div></div>"
     [:div {}] "<div></div>"
-    
+
+    ;; Element with id
     [:div {:id nil}]   "<div></div>"
     [:div {:id ""}]    "<div id=\"\"></div>"
     [:div {:id "foo"}] "<div id=\"foo\"></div>"
     [:div {:id 0}]     "<div id=\"0\"></div>"
 
+    ;; Element with class
     [:div {:class nil}]   "<div></div>"
     [:div {:class ""}]    "<div class=\"\"></div>"
     [:div {:class "foo"}] "<div class=\"foo\"></div>"
     [:div {:class 0}]     "<div class=\"0\"></div>"
-    
+
+    ;; Element with id, class
     [:div {:id nil :class nil}]     "<div></div>"
     [:div {:id "foo" :class nil}]   "<div id=\"foo\"></div>"
     [:div {:id nil :class "foo"}]   "<div class=\"foo\"></div>"
     [:div {:id "foo" :class "foo"}] "<div id=\"foo\" class=\"foo\"></div>"
 
+    ;; Element with id, class, attr
     [:div {:foo "bar"}]                        "<div foo=\"bar\"></div>"
     [:div {:id "foo" :foo "bar"}]              "<div id=\"foo\" foo=\"bar\"></div>"
     [:div {:class "foo" :foo "bar"}]           "<div class=\"foo\" foo=\"bar\"></div>"
     [:div {:id "foo" :class "foo" :foo "bar"}] "<div id=\"foo\" class=\"foo\" foo=\"bar\"></div>"
 
+    ;; Element body
     [:div nil]               "<div></div>"
     [:div ""]                "<div></div>"
     [:div "foo"]             "<div>foo</div>"
     [:div {:id "foo"} "foo"] "<div id=\"foo\">foo</div>"
 
+    ;; Elements nested
     [:div [:p]]                  "<div><p></p></div>"
     [:div [:p "foo"]]            "<div><p>foo</p></div>"
     [:div [:p "foo"] [:p "bar"]] "<div><p>foo</p><p>bar</p></div>"
     [:div {:id 0}
      [:p {:id 1} "foo"]
      [:p {:id 2} "bar"]]         "<div id=\"0\"><p id=\"1\">foo</p><p id=\"2\">bar</p></div>"
-    ))
+    [:div {:id 0}
+     [:p {:id 1}
+      [:span {:id 2}
+       "foo"]]]                  "<div id=\"0\"><p id=\"1\"><span id=\"2\">foo</span></p></div>"))
 
 (deftest test-html-void
   (are [node s] (= (c/html node) s)
