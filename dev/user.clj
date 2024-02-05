fb917ae04c5a06728e1cc0c965b4d380f822e722
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2024-02-05 02:15:06 -0800

    alias examples in dev user ns

--- dev/user.clj
+++ dev/user.clj
@@ -7,11 +7,14 @@
             [selmer.parser :as selmer]
             [net.cgrand.enlive-html :as enlive]))
 
-(defmethod c/custom-element ::foo
+(defmethod c/alias-element ::foo
   [tag attrs content]
-  (println tag attrs content)
-  [:div.foo
-   "foo"])
+  [:div.foo attrs
+   [:p.bar content]])
+
+(defmethod c/alias-element ::item-link
+  [tag attrs content]
+  [:a.baz.buz attrs content])
 
 (def data
   {:title "Test Items"
@@ -39,9 +42,32 @@
                      :href  (str "/item/" (:uuid m))}
          (:name m)]])]]])
 
+(defn page-alias
+  [data]
+  [:html {:lang "en"}
+   [:head
+    [:link {:href "/foobar1" :rel "stylesheet"}]
+    [:link {:href "/foobar2" :rel "stylesheet"}]
+    [:link {:href "/foobar3" :rel "stylesheet"}]
+    [:link {:href "/foobar4" :rel "stylesheet"}]
+    [:title (:title data)]]
+   [:body
+    [:h1 (:title data)]
+    [:div
+     (for [m (:items data)]
+       [:div.foobar
+        [::item-link {:id    (:uuid m)
+                      :class (:type m)
+                      :href  (str "/item/" (:uuid m))}
+         (:name m)]])]]])
+
 (defn chassis-page
   [data]
   (c/html [c/doctype-html5 (page data)]))
+
+(defn chassis-page-alias
+  [data]
+  (c/html [c/doctype-html5 (page-alias data)]))
 
 (defn hiccup-page
   [data]
